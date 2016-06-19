bba53783a42a26f74d1be03ba93078881a3e55ca
Trace Harris <traceh@whiletrace.com>
2016-06-18 21:18:25 -0700

    simple debugging

--- Beatbox_app/src/redux/modules/play.js
+++ Beatbox_app/src/redux/modules/play.js
@@ -1,35 +1,31 @@
-
-
 // set constants that are going to be used in the actions that will be dispatched to the reducer
 export const PLAY = 'PLAY'
-
- 
- // create actions
-
-export function togglePlay (IsPlaying){
-	return {
-		type: PLAY,
-		payload: IsPlaying
-	}
-
+export const MOUSE_OVER = 'MOUSE_OVER'
+export const MOUSE_LEAVE = 'MOUSE_LEAVE'
+export const CLICK = 'CLICK'
+export const DBL_CLICK = 'DBL_CLICK'
+// create actions
+export function togglePlay (IsPlaying) {
+  return {
+    type: PLAY,
+    payload: IsPlaying
+  }
 }
-
 // ------------------------------------
 // Action Handlers
 // ------------------------------------
 const ACTION_HANDLERS = {
-  [mouseover]: (state, action) => { return { IsPlaying: true } },
-  [mouseleave]: (state, action) => { return { IsPlaying: false } },
-  [click]: (state, action) => { return { IsPlaying: true } },
-  [dblclick]: (state, action) => { return { IsPlaying: false } }
+  [MOUSE_OVER]: (state, action) => { return { IsPlaying: true } },
+  [MOUSE_LEAVE]: (state, action) => { return { IsPlaying: false } },
+  [CLICK]: (state, action) => { return { IsPlaying: true } },
+  [DBL_CLICK]: (state, action) => { return { IsPlaying: false } }
 }
-
-	// ------------------------------------
+// ------------------------------------
 // Reducer
 // ------------------------------------
 const initialState = {IsPlaying: false}
 export default function BeatboxReducer (state = initialState, action) {
   const handler = ACTION_HANDLERS[action.type]
-  //shorthand if else statement
+  // shorthand if else statement
   return handler ? handler(state, action) : state
 }
